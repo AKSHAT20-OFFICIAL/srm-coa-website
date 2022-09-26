3abfa87d0ad891cc356d6053262b9f4f978a97f4
RishikaSharda <101321178+RishikaSharda@users.noreply.github.com>
2022-09-26 20:35:16 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Faculty from './components/Faculty';
 import Header from './components/Header';
 import Syllabus from './components/Syllabus';
+import pic from './components/images/img1.jpg';
+import pic1 from './components/images/img1.jpg';
+import pic2 from './components/images/img1.jpg';
 
 function App() {
   return (
@@ -9,6 +12,16 @@ function App() {
 
     <Header/>
     {/* <Faculty/> */}
+    <div className='inline-grid grid-cols-3'>
+       <img src={pic} height="70%" width="70%" className='m-10'/>
+       <img src={pic} height="70%" width="70%" className='m-10'/>
+       <img src={pic} height="70%" width="70%" className='m-10'/>
+
+       
+    </div>
+    
+   
+   
     
     </>
   );
