baa9f3ab3946512879abfcbbac622d6cc2a3c3ba
AKSHAT20-OFFICIAL <72640097+AKSHAT20-OFFICIAL@users.noreply.github.com>
2022-09-27 09:24:03 +0530

    changes

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -16,7 +16,7 @@ const MenuItems = ({ showMenu, active }) => {
                 <Link to='/syllabus'>Syllabus</Link>
             </li>
             <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
-                <Link to='/'>Notes</Link>
+                <Link to='/units'>Notes</Link>
             </li>
             <li className="text-gray-300 hover:bg-blue-400 hover:text-white px-4 py-2 rounded-md font-medium">
                 <Link to='/'>Register</Link>
